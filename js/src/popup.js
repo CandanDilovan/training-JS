198173418171756b555967a104bb55f55138d89a
unknown <candandilovan@gmail.com>
2024-06-03 01:15:28 +0200

    popup wip

--- js/src/popup.js
+++ js/src/popup.js
@@ -1,7 +1,22 @@
+const pop = document.querySelector(".register");
+const popbox = document.querySelector(".registration_container");
+const quitbox = document.querySelector(".oui");
 
+pop.onclick = () => {
+	registration_container.classList.add('on');
+	ft_overlay()
+}
 
-function ft_popup(){
-	var pop = document.getElementById("registration");
-	pop.classList.toggle("show");
-	console.log("clicked");
+quitbox.onclick = () => {
+	registration_container.classList.remove('on');
+	ft_overlay()
+}
+
+function ft_overlay()
+{
+	var over = document.getElementById('overlay');
+	if (over.style.display == 'none')
+		over.style.display = 'block';
+	else if (over.style.display == 'block')
+		over.style.display = 'none';
 }
